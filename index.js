fe526026d92d4d66e39497cc9fb86678bf6d6080
Simon Coffey <simon@urbanautomaton.com>
2026-07-17 12:45:47 +0100

    Check ably 2.24.0
    Same behaviour.

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ const { Rest, Realtime } = require('ably');
 
 const rest = new Rest(process.env.KEY);
 
-rest.auth.requestToken({ clientId: 'tls-proxy-test' }, (error, details) => {
-  if (error) {
-    console.error('AUTH ERROR: ', error);
-  } else {
-    const realtime = new Realtime({
-      // realtimeHost: 'realtime.ably.io',
-      realtimeHost: 'ably.tv.nevaya.net',
-      logLevel: 4,
-      tokenDetails: details,
-    });
+const run = async () => {
+  const details = await rest.auth.requestToken({ clientId: 'tls-proxy-test' })
 
-    const receiver_channel = realtime.channels.get('test');
-    receiver_channel.subscribe(console.log);
-  }
-});
+  const realtime = new Realtime({
+    // realtimeHost: 'realtime.ably.io',
+    realtimeHost: 'ably.tv.nevaya.net',
+    logLevel: 4,
+    tokenDetails: details,
+  });
+
+  const receiver_channel = realtime.channels.get('test');
+  receiver_channel.subscribe(console.log);
+}
+
+run();
