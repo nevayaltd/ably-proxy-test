3fe024450024cff67d11406ceb5d20fbed24dd78
Simon Coffey <simon@urbanautomaton.com>
2026-07-29 17:28:37 +0100

    Check fallback proxy

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ const { Rest, Realtime } = require('ably');
 const rest = new Rest(process.env.KEY);
 const connection = process.env.CONNECTION;
 const clientId = `nevaya-${connection}`;
-let realtimeHost;
+let options;
 
 if (connection === 'proxy') {
-  realtimeHost = 'ably.tv.nevaya.net';
+  options = {
+    realtimeHost: 'ably.tv.nevaya.net',
+    fallbackHosts: ['ably-fallback.tv.nevaya.net'],
+  }
 } else if (connection === 'direct') {
-  realtimeHost = 'realtime.ably.io';
+  options = {
+    realtimeHost: 'main.realtime.ably.net',
+    fallbackHosts: ['main.a.fallback.ably-realtime.com'],
+  }
 } else {
   throw new Error(`Unrecognised CONNECTION=${connection}`);
 }
@@ -17,10 +23,11 @@ const run = async () => {
   const details = await rest.auth.requestToken({ clientId })
 
   const realtime = new Realtime({
-    realtimeHost,
+    ...options,
     logLevel: 4,
     tokenDetails: details,
   });
+  realtime.connection.on('failed', console.error);
 
   const receiver_channel = realtime.channels.get('test');
   receiver_channel.subscribe(console.log);
